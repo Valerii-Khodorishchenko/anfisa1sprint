d80242aca9987d747c34ab702bbba37df77738e8
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-03-11 14:34:34 +0500

    final

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -3,18 +3,21 @@ from django.shortcuts import render
 ice_cream_catalog = [
     {
         'id': 0,
+        'img_path': 'img/classic_ice_cream.png',
         'title': 'Классический пломбир',
         'description': 'Настоящее мороженое, для истинных ценителей вкуса. '
                        'Если на столе появляется пломбир — это не надолго.',
     },
     {
         'id': 1,
+        'img_path': 'img/grasshoppers_ice_cream.png',
         'title': 'Мороженое с кузнечиками',
         'description': 'В колумбийском стиле: мороженое '
                        'с добавлением настоящих карамелизованных кузнечиков.',
     },
     {
         'id': 2,
+        'img_path': 'img/cheddar_ice_cream.png',
         'title': 'Мороженое со вкусом сыра чеддер',
         'description': 'Вкус настоящего сыра в вафельном стаканчике.',
     },
